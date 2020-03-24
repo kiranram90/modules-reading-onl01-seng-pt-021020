984a85c39c21405a01f11e4dbc78f1a49b563711
kiran ram <kram@lions.molloy.edu>
2020-03-24 04:05:53 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,7 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-  include Dance
+  include FancyDance::InstanceMethods
   extend FancyDance::ClassMethods
  
   attr_accessor :name
